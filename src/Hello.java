e2374cb4a963544a8ebf0229190052c6185aedac
Mohan Uppaluri <mohanuppaluri@gmail.com>
2022-07-27 15:56:13 -0400

    Initial Commit

---
+++ src/Hello.java
@@ -0,0 +1,12 @@
+public class Hello {
+    public static void main(String[] args) {
+
+        System.out.println("Om Varahi Mataye Namah");
+
+        int myFirstNumber = 5; //Declaration Statement
+
+        System.out.println("myFirstNumber");
+        System.out.println(myFirstNumber);
+
+    }
+}
