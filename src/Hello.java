c718fabf42f7852888d38c90e917d22374b69dd8
Mohan Uppaluri <mohanuppaluri@gmail.com>
2022-08-09 13:24:38 -0400

    Chapter 24 Finished

--- src/Hello.java
+++ src/Hello.java
@@ -1,12 +1,16 @@
-public class Hello {
-    public static void main(String[] args) {
+    public class Hello {
+        public static void main(String[] args) {
 
-        System.out.println("Om Varahi Mataye Namah");
+            System.out.println("Om Varahi Mataye Namah");
 
-        int myFirstNumber = 5; //Declaration Statement
+            int myFirstNumber = (10+5)+(2*10); //Declaration Statement
+            int mySecondNumber = 12;
+            int myThirdNumber = myFirstNumber * 2;
 
-        System.out.println("myFirstNumber");
-        System.out.println(myFirstNumber);
+            int myTotal = myFirstNumber + mySecondNumber + myThirdNumber;
 
+            int myLastOne =  1000 - myTotal;
+
+            System.out.println(myLastOne);
+        }
     }
-}
